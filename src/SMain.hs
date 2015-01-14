37775b20a1c7ba06117e612eb788c72cb30c7a65
Siwei Yang <yangsiwei880813@gmail.com>
2015-01-13 22:26:40 -0500

    add token builder; add test files loader for final test

--- src/SMain.hs
+++ src/SMain.hs
@@ -1,17 +1,38 @@
 module Main where
 
-import Data.List.Split(splitOn)
+import Lexical
+import Scanner
+
 import Data.Char(readLitChar)
 import Text.ParserCombinators.ReadP(eof, many, ReadP, readS_to_P, readP_to_S)
+
+import System.Directory
 
 strParser :: ReadP String
 strParser = do
     str <- many (readS_to_P readLitChar)
     eof
     return str
---trans :: String -> [(String, String)]
 trans :: String -> String
 trans = fst . head . (readP_to_S strParser)
+-----------------------------------------------------------------------------------------------------------------------------
+fixLength = (build scanKeyword) <=> (build scanOperator) <=> (build scanSeparator) <=> (build scanBool) <=> (build scanNull) :: Lex
+literal = (build scanChar) <=> (build scanString) <=> (build scanDecimalInteger) :: Lex
+leftover = (build scanIdentifier) <=> (build scanEolComment) <=> (build scanBlockComment) <=> (build scanWhitespace) :: Lex
+lexer = fixLength <|> literal <|> leftover
+-------------------------------------- Helpers ------------------------------------------------------------------------------
+testTokens :: IO [(String, String)]
+testTokens = do
+    f <- readFile "res/token.test"
+    let nl = zip [0..] (lines f)
+    return (zip [trans t | (n, t) <- nl, mod n 2 == 0] [t | (n, t) <- nl, mod n 2 == 1])
+
+testFiles :: IO [(String, String)]
+testFiles = do
+    f <- getDirectoryContents "../assignment_testcases/a1"
+    let files = ["../assignment_testcases/a1/" ++ file | file <- f, file /= ".", file /= ".."]
+    contents <- mapM readFile files
+    return (zip contents files)
 
 printPairs :: [(String, String)] -> IO()
 printPairs ((lexeme, token):ls) = do
@@ -21,10 +42,17 @@ printPairs ((lexeme, token):ls) = do
     then return ()
     else printPairs ls
 
+lexerRunner :: [(String, String)] -> IO()
+lexerRunner ((lexeme, token):ls) = do
+    putStr (show (lexeme, token))
+    putStr " => "
+    putStrLn (show (lexeme |> lexer))
+    if ls == []
+    then return ()
+    else lexerRunner ls
+-----------------------------------------------------------------------------------------------------------------------------
 main :: IO()
 main = do
-    f <- readFile "res/token.test"
-    let nl = zip [0..] (lines f)
-    let pairs = zip [trans t | (n, t) <- nl, mod n 2 == 0] [t | (n, t) <- nl, mod n 2 == 1]
-    printPairs pairs
+    pairs <- testTokens
+    lexerRunner pairs
     --putStrLn (show pairs)
