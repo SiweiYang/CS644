cf98d27812e58629925bc6e004cc65c2c8656c97
Jian Li <jian.li@uwaterloo.ca>
2015-02-01 23:44:30 -0500

    update EXP.hs

--- src/EXP.hs
+++ src/EXP.hs
@@ -8,12 +8,20 @@ import Parser
 -- operator special case := cast where we treat as binary
 data Expression = Unary { op :: String, expr :: Expression }
 				| Binary { op :: String, exprL :: Expression, exprR :: Expression }
-				| Value { expr :: Expression }
 				| Attribute { struct :: Expression, field :: Expression }
 				| Name { name :: Expression, identifier :: String }
 				| ID { identifier :: String }
 				| Array { name :: Expression, index :: Expression} 
+				| NewArray { arraytype :: String, dimexprs :: Expression, dims :: Expression}
+				| This
+				| Dimension { left :: Expression, index :: Expression }
+				| Value { valuetype :: String, value :: String }
+				| NewObject { classtype :: Expression, arguments :: Expression}
+				| Arguments { left :: Expression, expr :: Expression}
+				| FunctionCall { func :: Expression, arguments :: Expression}
+				| Cast { casttype :: Expression, dims :: Expression, expr :: Expression}
 				| Empty
+
 
 -- for . access, need to unify qualified name and field access
 -- a.b() is parsed in weird way, or maybe not
@@ -28,34 +36,51 @@ data Expression = Unary { op :: String, expr :: Expression }
 
 buildExp :: AST -> Expression
 buildExp ast = case (name ast) of
-					"Expression" -> buildExp singleton
-					"ConditionalExpression" -> buildExp singleton
-					"Assignment" -> Binary opEQ (buildExp lhs) (buildExp expr)
-					"ConditionalOrExpression" -> if (null condOr) then (buildExp singleton) else (Binary (op 1) (buildExp (head condOr)) (buildExp (head condAnd)))
-					"LeftHandSide" -> buildExp singleton
-					"ConditionalAndExpression" -> if (null condAnd) then (buildExp singleton) else (Binary (op 1) (buildExp (head condAnd)) (buildExp (head equal)))
-					"Name" -> buildExp singleton	 
-					"FieldAccess" -> Attribute (buildExp (head primary)) (buildExp (head identifier))
+					"AdditiveExpression" -> if (null additive) then (buildExp (head multiplicative)) else (Binary (tk 1) (buildExp (head additive)) (buildExp (head multiplicative)))
+					"ArgumentList" -> Arguments (if (null args) then Empty else (buildExp (head args))) (buildExp (head expr))
 					"ArrayAccess" -> Array (buildExp (head (if (null name) then (priarray) else name))) (buildExp (head expr))
-					"SimpleName" -> buildExp (head identifier)
-					"IDENTIFIER" -> buildID ast
-					"QualifiedName" -> Name (buildExp (head name)) (buildExp (head identifier))
-					"EqualityExpression" -> if (null equal) then (buildExp (head relational)) else (Binary (op 1) (buildExp (head equal)) (buildExp (head relational)))
-					"RelationalExpression" -> if (null relational) then (buildExp (head additive)) else (if (null additive) then (Binary (op 1) (buildExp (head relational)) (buildExp (head reftype))) else (Binary (op 1) (buildExp (head relational)) (buildExp (head additive))))
+					"ArrayCreationExpression" -> NewArray "" (buildExp (head dimexprs)) (if (null dims) then Empty else (buildExp (head dims))) -- bug here!
+					"Assignment" -> Binary (tk 1) (buildExp lhs) (buildExp expr)
+					"ConditionalExpression" -> buildExp singleton
+					"ConditionalAndExpression" -> if (null condAnd) then (buildExp singleton) else (Binary (tk 1) (buildExp (head condAnd)) (buildExp (head equal)))
+					"ConditionalOrExpression" -> if (null condOr) then (buildExp singleton) else (Binary (tk 1) (buildExp (head condOr)) (buildExp (head condAnd)))
+					"ClassInstanceCreationExpression" -> NewObject (buildExp (head classtype)) (if (null args) then Empty else (buildExp (head args)))
+					"ClassOrInterfaceType" ->  
+					"Dims" -> if (null dim) then (Dimension Empty Empty) else (Dimension (buildExp (head dim)) Empty)
+					"DimExpr" -> buildExp (head expr))
+					"DimExprs" -> if (null dimexprs) then (Empty Dimension (buildExp (head dimexpr))) else (Dimension (buildExp (head dimexprs)) (buildExp (head dimexpr)))
+					"EqualityExpression" -> if (null equal) then (buildExp (head relational)) else (Binary (tk 1) (buildExp (head equal)) (buildExp (head relational)))
+					"Expression" -> buildExp singleton
+					"FieldAccess" -> Attribute (buildExp (head primary)) (buildExp (head identifier))
+					"LeftHandSide" -> buildExp singleton
+					"Literal" -> Value (name singleton) (tk 0)
+					"MethodInvocation" -> FunctionCall (buildExp (choose 0)) (if (null args) then Empty else (buildExp (head args)))
+					"MultiplicativeExpression" -> if (null multiplicative) then (buildExp singleton) else (Binary (tk 1) (buildExp (head multiplicative)) (buildExp (head unary)))
+					"Name" -> buildExp singleton
 					"Primary" -> buildExp singleton
-					"PrimaryNoNewArray" ->
+					"PrimaryNoNewArray" -> if (null expr) (buildExp singleton) then (buildExp (head expr))
+					"QualifiedName" -> Name (buildExp (head name)) (buildExp (head identifier))
+					"RelationalExpression" -> if (null relational) then (buildExp (head additive)) else (if (null additive) then (Binary (tk 1) (buildExp (head relational)) (buildExp (head reftype))) else (Binary (tk 1) (buildExp (head relational)) (buildExp (head additive))))
+					"SimpleName" -> buildExp (head identifier)
+					"UnaryExpression" -> if (null unary) then (buildExp singleton) else (Unary (tk 0) (buildExp (head unary)))
+					"UnaryExpressionNotPlusMinus" -> if (null unary) then (buildExp singleton) else (Unary (tk 0) (buildExp (head unary))) 
+					"PostfixExpression" -> buildExp singleton
+					"CastExpression" -> if (null unary) then (if (null expr) then (Cast (buildExp (head name)) (buildExp (head dims)) (buildExp (head unarynotpm))) else (Cast (buildExp (head expr)) Empty (buildExp (head unarynotpm)))) else (Cast (buildExp (head pritype)) (if (null dims) then Empty else (buildExp (head dims))) (buildExp (head unary)))
+					"PrimitiveType" -> 
 					"ReferenceType" -> 
-					"ArrayCreationExpression" -> 
+					"IDENTIFIER" -> buildToken ast
+					"KEYWORD_THIS" -> This
+
 	where
 		[singleton] = production ast
 
 		lhs = head (findProd "LeftHandSide" ast)
-		opEQ = buildOperator (head (findProd "OPERATOR_=" ast))
 		expr = head (findProd "Expression" ast)
 
 		condAnd = findProd "ConditionalAndExpression" ast
 		condOr = findProd "ConditionalOrExpression" ast
-		op = \k -> buildOperator expandSingle ((production ast) !! k)
+		choose = \k -> (production ast) !! k
+		tk = \k -> buildToken expandSingle (choose k)
 
 		equal = findProd "EqualityExpression" ast
 		name = findProd "Name" ast
@@ -66,17 +91,25 @@ buildExp ast = case (name ast) of
 
 		relational = findProd "RelationalExpression" ast
 		additive = findProd "AdditiveExpression" ast
+		multiplicative = findProd "MultiplicativeExpression" ast
+		unary = findProd "UnaryExpression" ast
+		unarynotpm = findProd "UnaryExpressionNotPlusMinus" ast
+
 		reftype = findProd "ReferenceType" ast
+		pritype = findProd "PrimitiveType" ast
+
+		dimexprs = findProd "DimExprs" ast
+		dimexpr = findProd "DimExpr" ast
+		dim = findProd "Dims" ast
+
+		classtype = findProd "ClassType" ast
+		args = findProd "ArgumentList" ast
 
 
 --------------------------------------------------------
 
-buildOperator :: AST -> String
-buildOperator ast = lexeme (fst (content ast))
-
-buildID :: AST -> Expression
-buildID ast = ID (lexeme (fst (content ast)))
-
+buildToken :: AST -> String
+buildToken ast = lexeme (fst (content ast))
 
 findProd :: String -> AST -> [AST]
 findProd nm ast = filter (\ast -> (name ast) == nm) (production ast)
