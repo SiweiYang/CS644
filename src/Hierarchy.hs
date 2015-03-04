003bb53227dd3013bab579ff1a4b5905fc5d574a
Justin Vanderheide <justin@justinvanderheide.com>
2015-03-03 23:44:53 -0500

    Check to make sure interfaces implement only interfaces that are not themselves

--- src/Hierarchy.hs
+++ src/Hierarchy.hs
@@ -37,6 +37,7 @@ checkImplements _ _ = Nothing
 checkExtends :: CompilationUnit -> TypeNode -> HierarchyError
 checkExtends unit@(Comp _ _ (CLS _ clsName (Just extendee) _ _ _ _ _) _) typeDB
   | not extendedNodeExists = Just $ "Class " ++ clsName ++ " tried to extend non-existent class " ++ (show extendee)
+  | isInterface extendedSymbol = Just $ "Class " ++ clsName ++ " cannot extend interface " ++ (localName extendedSymbol)
   | "final" `elem` (symbolModifiers extendedSymbol) = Just $ "Class " ++ clsName ++ " cannot extend final class " ++ (localName extendedSymbol)
   | extendedName == ownName = Just $ "Class " ++ clsName ++ " cannot extend itself"
   | otherwise = Nothing
@@ -46,5 +47,14 @@ checkExtends unit@(Comp _ _ (CLS _ clsName (Just extendee) _ _ _ _ _) _) typeDB
         extendedNode = getTypeEntry typeDB (head extendedName)
         extendedNodeExists = isJust extendedNode
         extendedSymbol = symbol . fromJust $ extendedNode
-        -- extendedInterfaces = filter isInterface extendedSymbols
+checkExtends unit@(Comp _ _ (ITF _ itfName extended _ _) _) typeDB
+  | not . null $ extendedClasses = Just $ "Interface " ++ itfName ++ " cannot extend class " ++ (localName $ head extendedClasses)
+  | nub extendedClasses /= extendedClasses = Just $ "Interface " ++ itfName ++ " extends the same interface twice"
+  | ownName `elem` extendedNames = Just $ "Interface " ++ itfName ++ " cannot extend itself"
+  where unitImports = visibleImports unit
+        ownName = traverseTypeEntryWithImports typeDB unitImports [itfName]
+        extendedNames = map (traverseTypeEntryWithImports typeDB unitImports) extended
+        extendedNodes = mapMaybe (getTypeEntry typeDB) (map head extendedNames)
+        extendedSymbols = map symbol extendedNodes
+        extendedClasses = filter isClass extendedSymbols
 checkExtends _ _ = Nothing
