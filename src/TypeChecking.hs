f4f95b5cd49f6a1f3825e2d18f8b648eada83490
Jian Li <lijianxp2005@gmail.com>
2015-03-11 13:45:30 -0400

    use higherInChain

--- src/TypeChecking.hs
+++ src/TypeChecking.hs
@@ -7,41 +7,49 @@ import TypeLinking
 import Hierarchy
 import Data.Maybe
 
-assignConversion :: CompilationUnit -> TypeNode -> Type -> Type -> Bool
+assignConversion :: TypeNode -> Type -> Type -> Bool
 
-assignConversion _ _ _ (Object (Name ["String"])) = True
-assignConversion _ _ TypeVoid t = t == TypeVoid
-assignConversion _ _ TypeNull (Object _) = True
-assignConversion _ _ TypeNull (Array _) = True
+assignConversion _ _ (Object (Name ["String"])) = True
+assignConversion _ TypeVoid t = t == TypeVoid
+assignConversion _ TypeNull (Object _) = True
+assignConversion _ TypeNull (Array _) = True
 --assignConversion TypeNull TypeNull = True
-assignConversion _ _ TypeByte t = elem t [TypeByte, TypeShort, TypeInt]
-assignConversion _ _ TypeShort t = elem t [TypeShort, TypeInt]
-assignConversion _ _ TypeInt t = t == TypeInt
-assignConversion _ _ TypeBoolean t = t == TypeBoolean
-assignConversion _ _ TypeChar t = elem t [TypeChar, TypeInt]
-assignConversion _ _ TypeString t = t == TypeString
-assignConversion _ _ (Array _) (Object (Name ["Object"])) = True
-assignConversion _ _ (Array _) (Object (Name ["Cloneable"])) = True
-assignConversion _ _ (Array _) (Object (Name ["java", "io", "Serializable"])) = True
-assignConversion unit typeDB (Array x@(Object _)) (Array y@(Object _)) = assignConversion unit typeDB x y
-assignConversion _ _ (Array x) (Array y) = x == y && elem x [TypeByte, TypeShort, TypeInt, TypeBoolean, TypeChar, TypeString]
-assignConversion _ _ (Object _) (Object (Name ["Object"])) = True
-assignConversion unit typeDB x@(Object _) y@(Object _)
+assignConversion _ TypeByte t = elem t [TypeByte, TypeShort, TypeInt]
+assignConversion _ TypeShort t = elem t [TypeShort, TypeInt]
+assignConversion _ TypeInt t = t == TypeInt
+assignConversion _ TypeBoolean t = t == TypeBoolean
+assignConversion _ TypeChar t = elem t [TypeChar, TypeInt]
+assignConversion _ TypeString t = t == TypeString
+assignConversion _ (Array _) (Object (Name ["Object"])) = True
+assignConversion _ (Array _) (Object (Name ["Cloneable"])) = True
+assignConversion _ (Array _) (Object (Name ["java", "io", "Serializable"])) = True
+assignConversion typeDB (Array x@(Object _)) (Array y@(Object _)) = assignConversion typeDB x y
+assignConversion _ (Array x) (Array y) = x == y && elem x [TypeByte, TypeShort, TypeInt, TypeBoolean, TypeChar, TypeString]
+assignConversion _ (Object _) (Object (Name ["Object"])) = True
+assignConversion unit typeDB (Object (Name x)) (Object (Name y))
 	| x == y = True
- 	| otherwise = isExtentedImplemented unit typeDB x y
-assignConversion _ _ _ _ = False
+ 	| otherwise = 
+ 	where
+ 		symbolX = symbol . fromJust $ getTypeEntry typeDB x
+ 		symbolY = symbol . fromJust $ getTypeEntry typeDB y
 
-castConversion :: CompilationUnit -> TypeNode -> Type -> Type -> Bool
-castConversion _ _ TypeVoid t = False
-castConversion _ _ TypeBoolean t = t == TypeBoolean
-castConversion _ _ TypeNull t = t == TypeNull
-castConversion _ _ TypeByte t = elem t [TypeByte, TypeShort, TypeInt]
-castConversion _ _ TypeShort t = elem t [TypeShort, TypeInt]
-castConversion _ _ TypeInt t = t == TypeInt
-castConversion _ _ TypeChar t = elem t [TypeChar, TypeInt]
-castConversion _ _ TypeString t = t == TypeString
-castConversion unit typeDB x y = assignConversion unit typeDB x y
 
+
+assignConversion _ _ _ = False
+
+castConversion :: TypeNode -> Type -> Type -> Bool
+castConversion _ TypeVoid t = False
+castConversion _ TypeBoolean t = t == TypeBoolean
+castConversion _ TypeNull t = t == TypeNull
+castConversion _ TypeByte t = elem t [TypeByte, TypeShort, TypeInt]
+castConversion _ TypeShort t = elem t [TypeShort, TypeInt]
+castConversion _ TypeInt t = t == TypeInt
+castConversion _ TypeChar t = elem t [TypeChar, TypeInt]
+castConversion _ TypeString t = t == TypeString
+castConversion typeDB x y = assignConversion typeDB x y
+
+
+{-
 
 isExtentedImplemented :: CompilationUnit -> TypeNode -> Type -> Type -> Bool
 isExtentedImplemented unit typeDB (Object (Name x)) (Object (Name y)) = elem yNode xEI
@@ -55,5 +63,6 @@ isExtentedImplemented unit typeDB (Object (Name x)) (Object (Name y)) = elem yNo
 		xEI = getClassHierarchy xUnit typeDB
 isExtentedImplemented _ _ _ _ = error "ExtendedImplemented: otherwise"
 
+-}
 
 
